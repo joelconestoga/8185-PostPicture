9a260d5edb63bb47e5515b1472d3d46d28c1f2a0
joel matsu <matsu.joel@gmail.com>
2017-05-30 19:41:22 -0400

    created web-form project

---
+++ PostPicture/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_8185_PostPicture.Startup))]
+namespace _8185_PostPicture
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
